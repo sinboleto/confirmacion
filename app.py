46f310c325a7426510b62fd09e781ac8510c7b09
sinboleto <santiago@sinboleto.com.mx>
2023-07-12 10:55:21 -0600

    Actualizacion codigo

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'  # Replace 'data.db' with your desired database name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To suppress a warning
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 # Create a model for your data
@@ -23,15 +26,14 @@ def index():
         name = request.form['name']
         email = request.form['email']
 
-        # Create a new Information object and save it to the database
         new_info = Information(name, email)
         db.session.add(new_info)
         db.session.commit()
 
-    # Fetch all the stored information from the database
     infos = Information.query.all()
 
     return render_template('index.html', infos=infos)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
