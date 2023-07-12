ab61d4bd883f87b52d3c8b3f28bb114001d86af1
sinboleto <santiago@sinboleto.com.mx>
2023-07-12 11:43:18 -0600

    Cambio estructura chatbot

--- app.py
+++ app.py
@@ -3,62 +3,109 @@ from flask import Flask, render_template, request, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
+
+from dotenv import load_dotenv
+import os
+
 app = Flask(__name__)
+
+port = int(os.environ.get('PORT', 5000))
+
+# Variables base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Twilio credentials
+account_sid = os.environ.get('ACCOUNT_SID')
+auth_token = os.environ.get('AUTH_TOKEN')
+twilio_phone_number = os.environ.get('TWILIO_PHONE_NUMBER')
+
+# Create Twilio client
+client = Client(account_sid, auth_token)
+
 
 class Information(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(50))
+    # name = db.Column(db.String(50))
+    # email = db.Column(db.String(50))
+    info = db.Column(db.String(500))
 
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
+    def __init__(self, id, info):
+        self.id = id
+        self.info = info
+        # self.email = email
 
+# Inicio conversación
+message = client.messages.create(
+  from_ = f'whatsapp:{twilio_phone_number}',
+  body = 'Hola',
+  to = 'whatsapp:+5215551078511'
+)
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def webhook():
     if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
 
-        new_info = Information(name, email)
+        # incoming_message = request.values.get('Body', '').lower()
+        incoming_message = request.values
+        response = MessagingResponse()
+
+        new_info = Information(id, incoming_message)
         db.session.add(new_info)
         db.session.commit()
 
-    infos = Information.query.all()
+        return str(response)
+    
+    else:
 
-    return render_template('index.html', infos=infos)
+        return 'Inicio exitoso'
+
+# @app.route('/input', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         email = request.form['email']
+
+#         new_info = Information(id, name, email)
+#         db.session.add(new_info)
+#         db.session.commit()
+
+#     infos = Information.query.all()
+
+#     return render_template('index.html', infos=infos)
 
 @app.route('/view')
 def view():
     infos = Information.query.all()
     return render_template('view.html', infos=infos)
 
-@app.route('/export', methods=['POST'])
-def export():
-    infos = Information.query.all()
+# @app.route('/export', methods=['POST'])
+# def export():
+#     infos = Information.query.all()
 
-    # Create the CSV file
-    with open('data.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Name', 'Email'])
-        for info in infos:
-            writer.writerow([info.name, info.email])
+#     # Create the CSV file
+#     with open('data.csv', 'w', newline='') as csvfile:
+#         writer = csv.writer(csvfile)
+#         writer.writerow(['ID','Name', 'Email'])
+#         for info in infos:
+#             writer.writerow([info.id, info.name, info.email])
 
-    # Send the CSV file as a response for download
-    with open('data.csv', 'r') as f:
-        csv_data = f.read()
+#     # Send the CSV file as a response for download
+#     with open('data.csv', 'r') as f:
+#         csv_data = f.read()
 
-    response = Response(csv_data, mimetype='text/csv')
-    response.headers.set('Content-Disposition', 'attachment', filename='data.csv')
+#     response = Response(csv_data, mimetype='text/csv')
+#     response.headers.set('Content-Disposition', 'attachment', filename='data.csv')
     
-    return response
+#     return response
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
