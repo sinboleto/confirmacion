7075480846de20b78619355cf0f2d6191d2be228
sinboleto <santiago@sinboleto.com.mx>
2023-07-12 11:08:17 -0600

    Agrega boton exportar BD

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, request
+import csv
+from flask import Flask, render_template, request, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To suppress a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-# Create a model for your data
 class Information(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
@@ -35,10 +35,25 @@ def index():
     return render_template('index.html', infos=infos)
 
 
-@app.route('/view')
-def view():
+@app.route('/export', methods=['POST'])
+def export():
     infos = Information.query.all()
-    return render_template('view.html', infos=infos)
+
+    # Create the CSV file
+    with open('data.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Name', 'Email'])
+        for info in infos:
+            writer.writerow([info.name, info.email])
+
+    # Send the CSV file as a response for download
+    with open('data.csv', 'r') as f:
+        csv_data = f.read()
+
+    response = Response(csv_data, mimetype='text/csv')
+    response.headers.set('Content-Disposition', 'attachment', filename='data.csv')
+    
+    return response
 
 
 if __name__ == '__main__':
