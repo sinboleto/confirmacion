8453962174b903ddfdf983cc2371d85b58203cba
sinboleto <santiago@sinboleto.com.mx>
2023-07-18 13:08:42 -0600

    Actualizacion logica confirmacion

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import csv
 from flask import Flask, render_template, request, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask import session
 
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
@@ -37,10 +38,15 @@ client = Client(account_sid, auth_token)
 
 class Information(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    info = db.Column(db.String(500), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+    question = db.Column(db.String(500), nullable=False)
+    answer = db.Column(db.String(500), nullable=False)
 
-    def __init__(self, info):
-        self.info = info
+    def __init__(self, phone_number, question, answer):
+        self.phone_number = phone_number
+        self.question = question
+        self.answer = answer
+
 
 # Inicio conversación
 @app.route('/start', methods=['GET'])
@@ -55,28 +61,69 @@ def inicio_conversacion():
     return 'Inicio'
 
 
-@app.route('/', methods=['GET', 'POST'])
+# @app.route('/', methods=['GET', 'POST'])
+# def webhook():
+#     if request.method == 'POST':
+        
+#         incoming_message = request.values
+#         incoming_message_body = request.values.get('Body', '').lower()
+
+#         response = MessagingResponse()
+#         texto_respuesta = f'Tu mensaje: "{incoming_message_body}"'
+#         response.message(texto_respuesta)
+
+#         new_info = Information(str(incoming_message))
+#         db.session.add(new_info)
+#         db.session.commit()
+
+#         print(str(incoming_message))
+
+#         return str(response)
+    
+#     else:
+
+#         return 'Inicio exitoso'
+
+@app.route('/', methods=['POST'])
 def webhook():
-    if request.method == 'POST':
+    incoming_message_body = request.values.get('Body', '').lower()
+    incoming_phone_number = request.values.get('From', '').lower()
 
-        incoming_message = request.values
-        incoming_message_body = request.values.get('Body', '').lower()
+    # Retrieve the current question index from the session
+    current_question_index = session.get('current_question_index', -1)
 
-        response = MessagingResponse()
-        texto_respuesta = f'Tu mensaje: "{incoming_message_body}"'
-        response.message(texto_respuesta)
+    response = MessagingResponse()
 
-        new_info = Information(str(incoming_message))
+    if current_question_index == -1:
+        # First response, initialize the sequence of questions
+        session['questions'] = [
+            "What is your name?",
+            "What is your age?",
+            "What is your favorite color?"
+        ]
+        current_question_index = 0
+        session['current_question_index'] = current_question_index
+
+    # Get the user's answer
+    user_answer = str(incoming_message_body)
+
+    if current_question_index >= len(session['questions']):
+        # No more questions, end the conversation
+        response.message("Thank you for answering all the questions.")
+    else:
+        # Save the question and answer in the database
+        current_question = session['questions'][current_question_index]
+        new_info = Information(incoming_phone_number, current_question, user_answer)
         db.session.add(new_info)
         db.session.commit()
 
-        print(str(incoming_message))
+        # Send the next question
+        current_question_index += 1
+        session['current_question_index'] = current_question_index
+        next_question = session['questions'][current_question_index]
+        response.message(next_question)
 
-        return str(response)
-    
-    else:
-
-        return 'Inicio exitoso'
+    return str(response)
 
 @app.route('/view')
 def view():
